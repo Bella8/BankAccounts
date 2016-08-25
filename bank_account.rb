3dd89947c5b00cecd9f7aa3f6d241ba25f00e8ec
Beylul <bkbreab8@gmail.com>
2016-08-24 20:11:19 -0700

    Finished with Wave two primary requrements

--- bank_account.rb
+++ bank_account.rb
@@ -1,30 +1,21 @@
+# require_relative 'owner'
+require 'csv'
 module Bank
   class Account
 
-    attr_accessor :withdraw, :deposit, :balance, :new_account, :id
+    attr_accessor :balance, :id, :open_date
 
-    def initialize(id, balance, message = "Assertion Failed")
-      @withdraw = 0
-      @deposit = 0
+    def initialize(id, balance, open_date)
+
+      @id = id
       @balance  = balance
-      @new_account = {}
-      @id = nil
+      @open_date = open_date
 
-      if balance > 0
-        return true
-      else raise Exception.new(message)
+      unless @balance >= 0
+        message = "Assertion Failed"
+        raise Exception.new(message)
       end
     end
-
-     #Alternative way of addding id and balance
-    # def new_account(id, balance)
-    #   if balance < 0
-    #     puts"Sorry you can't create a negative balance!"
-    #   else
-    #  @new_account[:id] = balance
-    #  @balance = balance
-    #   end
-    # end
 
     def current_balance
       return @balance
@@ -32,7 +23,7 @@ module Bank
 
     def withdraw_money(withdraw)
       new_balance = @balance - withdraw
-      if new_balance < 0
+      if new_balance <= 0
         puts "Sorry you can't withdraw an amount that is more than your balance!"
         puts @balance
       else
@@ -47,10 +38,43 @@ module Bank
       @balance = new_balance
       return @balance
     end
+
+    def self.all
+      account_info = []
+        CSV.read('support/accounts.csv').each do |line|
+          id = line[0].to_i
+          balance = line[1].to_i
+          open_date = line[2].to_i
+          #arrays of arrays
+          account_info<< self.new(id, balance, open_date)
+        end
+      return account_info
+    end
+
+    def self.find(id)
+      self.all.each do |line|
+        #id is at zero index
+        if line.id == id
+          return line
+        end
+      end
+    end
   end
 end
 
-c = Bank::Account.new("123", 50)
-puts c.withdraw_money(70)
-puts c.current_balance
-puts c.deposit_money(40)
+
+# test for wave two
+#   accounts = Bank::Account.all
+#   account = Bank::Account.find(1214)
+#   p account
+#   # puts accounts
+
+
+
+#test for Wave one
+  # c=  Bank::Account.new()
+
+  # c = Bank::Account.new("123", 50 )
+  # puts c.withdraw_money(70)
+  # puts c.current_balance
+  # puts c.deposit_money(40)
